acd7bea237e7e545a5ef24881ba825f89a4bb643
Sushmitha S <sushmitha9724@yahoo.com>
2022-11-22 18:08:53 +0530

    product

---
+++ BookStore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,105 @@
+﻿using BookStore.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BookStore.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+
+            IEnumerable<CoverType> objCoverTypeList = _unitOfWork.CoverType.GetAll();
+            return View(objCoverTypeList);
+        }
+       
+        //get
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new();
+            if (id == null || id == 0)
+            {
+                //create product
+                return View(product);
+            }
+            else
+            {
+                //update product
+            }
+            
+           
+            return View();
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(CoverType obj)
+        {
+           
+            if (ModelState.IsValid)
+            {
+
+                _unitOfWork.CoverType.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "CoverType updated successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+        }
+
+        //get
+        public IActionResult Delete(int? id)
+        {
+
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var CoverTypeFromDb = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+
+            if (CoverTypeFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(CoverTypeFromDb);
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _unitOfWork.CoverType.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+
+            if (ModelState.IsValid)
+            {
+
+                _unitOfWork.CoverType.Remove(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "CoverType Deleted successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+        }
+
+
+    }
+
+}
+
+
